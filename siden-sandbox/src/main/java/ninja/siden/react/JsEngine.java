2278558349f0f4a85dee64a722631d5399bf91e8
SATO taichi <ryushi@gmail.com>
2014-10-10 20:35:03 +0900

    server side rendering support with react.js

--- siden-sandbox/src/main/java/ninja/siden/react/JsEngine.java
+++ siden-sandbox/src/main/java/ninja/siden/react/JsEngine.java
@@ -18,37 +18,51 @@ package ninja.siden.react;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.logging.Logger;
 
+import javax.script.Bindings;
+import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 
 import ninja.siden.util.Io;
+import ninja.siden.util.Loggers;
 import ninja.siden.util.Supress;
 
 /**
  * @author taichi
  */
 public class JsEngine {
+	
+	static final Logger LOG = Loggers.from(JsEngine.class);
 
 	final ScriptEngineManager manager;
 	ScriptEngine engine;
+	Bindings global;
 
 	public JsEngine() {
 		manager = new ScriptEngineManager();
 	}
 
 	ScriptEngine newEngine() {
-		return manager.getEngineByName("nashorn");
+		return manager.getEngineByExtension("js");
 	}
 
 	public void initialize(List<Path> scripts) {
 		this.engine = newEngine();
-		eval("var global = this;");
-		scripts.stream().map(
-				p -> Io.using(() -> Files.newBufferedReader(p), engine::eval));
+		this.global = engine.getBindings(ScriptContext.ENGINE_SCOPE);
+		scripts.forEach(this::eval);
 	}
 
 	public Object eval(String script) {
-		return Supress.get(() -> engine.eval(script));
+		LOG.finest(() -> global.keySet().toString());
+		return Supress.get(() -> engine.eval(script, global));
+	}
+
+	public Object eval(Path path) {
+		LOG.finest(() -> global.keySet().toString());
+		return Io.using(() -> Files.newBufferedReader(path), r -> {
+			return engine.eval(r, global);
+		});
 	}
 }
